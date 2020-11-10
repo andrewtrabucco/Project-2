60061156f74ffb960729a90996236e39babcf471
Gina Dannibale <ginadannibale@gmail.com>
2020-11-09 20:26:28 -0600

    Caleb

--- controllers/calorieController.js
+++ controllers/calorieController.js
@@ -3,37 +3,37 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var calorie = require("../models/cat.js");
+var calorie = require("../models/index.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   calorie.all(function(data) {
     var hbsObject = {
-      cats: data
+      calories: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
-router.post("/api/cats", function(req, res) {
-  calorie.create([
-    "name", "sleepy"
+router.post("/api/food", function(req, res) {
+  calories.create([
+    "foodName", calories
   ], [
-    req.body.name, req.body.sleepy
+    req.body.foodName, req.body.calories
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/cats/:id", function(req, res) {
+router.put("/api/calories/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  cat.update({
-    sleepy: req.body.sleepy
+  calorie.update({
+    calories: req.body.calories
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
@@ -44,10 +44,10 @@ router.put("/api/cats/:id", function(req, res) {
   });
 });
 
-router.delete("/api/cats/:id", function(req, res) {
+router.delete("/api/calorie/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  cat.delete(condition, function(result) {
+  calorie.delete(condition, function(result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
