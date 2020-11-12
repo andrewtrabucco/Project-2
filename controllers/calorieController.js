b7bc0b5a50714f73043013a54f19db38d74465b8
Gina Dannibale <ginadannibale@gmail.com>
2020-11-11 18:45:09 -0600

    Changes

--- controllers/calorieController.js
+++ controllers/calorieController.js
@@ -3,11 +3,11 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (index.js) to use its database functions.
-var calorie = require("../models/index.js");
+var calories = require("../models/index.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  calorie.all(function(data) {
+  calories.all(function(data) {
     var hbsObject = {
       calories: data
     };
@@ -18,7 +18,7 @@ router.get("/", function(req, res) {
 
 
 router.post("/api/calories", function(req, res) {
-  calorie.create([
+  calories.create([
     "foodName", calories
   ], [
     req.body.foodName, req.body.calories
@@ -33,7 +33,7 @@ router.put("/api/calories/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  calorie.update({
+  calories.update({
     calories: req.body.calories
   }, condition, function(result) {
     if (result.changedRows == 0) {
@@ -48,7 +48,7 @@ router.put("/api/calories/:id", function(req, res) {
 router.delete("/api/calories/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  calorie.delete(condition, function(result) {
+  calories.delete(condition, function(result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
--- public/assets/js/calories.js
+++ public/assets/js/calories.js
@@ -1,31 +1,5 @@
-
-let unirest = require("unirest");
-let req = unirest("GET", "https://api.spoonacular.com/food/ingredients/search?query=" + foodID + "&apiKey=c1efb5fd0f5141858fc5b5f6a6b5ab85&includeNutrition=true");
-let req1 = unirest("GET", "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/ingredients/" + food + "/information");
-
-
-req.query({
-	"amount": "100",
-	"unit": "gram"
-});
-req1.query({
-	"amount": "100",
-	"unit": "gram"
-});
-
-req.headers({
-	"x-rapidapi-key": "c1efb5fd0f5141858fc5b5f6a6b5ab85",
-	"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-	"useQueryString": true
-});
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
-	console.log(res.body);
-});
-
-// RapidAPI API Call
-
-let queryURL = "https://api.spoonacular.com/food/ingredients/search?query=apple&apiKey=c1efb5fd0f5141858fc5b5f6a6b5ab85&includeNutrition=true";
+let queryURL = 'https://api.spoonacular.com/food/ingredients/search?query=' + foodID + '&apiKey=c1efb5fd0f5141858fc5b5f6a6b5ab85&includeNutrition=true';
+let queryURL1 = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/ingredients/' + food + '/information';
 
 function searchFood() {
 
@@ -40,7 +14,7 @@ function searchFood() {
 // Search Button Functionality (Imputs Text and Calls searchFood Function)
 $("#searchButton").on("click", function () {
     event.preventDefault();
-    let inputFood = $("#findtext").val().trim();
+    let food = $("#findtext").val().trim();
 
     searchFood(inputFood);
 });
