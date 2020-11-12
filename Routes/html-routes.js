1d457b21cd29f4d234409f08ba7c37cca0b4481c
Caleb Chaney <calebc347@gmail.com>
2020-11-11 20:20:04 -0600

    Added api-routes and updated models and html-routes

---
+++ Routes/html-routes.js
@@ -0,0 +1,11 @@
+
+module.exports = function (app) {
+
+    app.get("/", function (req, res) {
+        res.render("index");
+    });
+
+//     app.get("/calendar", function (req, res) {
+//         res.send("this will be the calendar");
+//     });
+// }
