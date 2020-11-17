2ef60dfbf23d8ce62301813a226461074df47a40
Caleb Chaney <calebc347@gmail.com>
2020-11-17 11:23:10 -0600

    Added ability to replace dislpayed list

--- public/assets/js/calories.js
+++ public/assets/js/calories.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
 // Search Button Functionality (Imputs text and calls searchFood function)
 $("#searchButton").on("click", function (event) {
     event.preventDefault();
-
+    $(".food-list").empty();
     let food = $("#foodTextEntry").val().trim();
 
     // console.log(food);
