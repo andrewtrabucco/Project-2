a7db44a6f58950bd09ef969702e7517be17ef3b2
Caleb Chaney <67514046+Cachamoe@users.noreply.github.com>
2020-11-16 20:14:28 -0600

    Update html-routes.js

--- Routes/html-routes.js
+++ Routes/html-routes.js
@@ -5,11 +5,3 @@ module.exports = function (app) {
         res.render("index");
     });
 }
-
-<<<<<<< HEAD
-=======
-//     app.get("/calendar", function (req, res) {
-//         res.send("this will be the calendar");
-//     });
-// }
->>>>>>> parent of de753b9... Merge branch 'main' into Caleb-Branch
